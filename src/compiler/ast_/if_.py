590db6510bc3b16ea22c8367af34db60d3eed229
Alex M <you@example.com>
2022-05-27 16:02:15 +0700

    intermediate

--- src/compiler/ast_/if_.py
+++ src/compiler/ast_/if_.py
@@ -34,6 +34,18 @@ class If(Node):
     def __repr__(self) -> str:
         return str(f"<If {self.location})>")
 
+    def num_out_ports(self):
+        n_then = self.then.num_out_ports()
+        n_else = self.else_.num_out_ports()
+        num_elses_ports = [elseif.num_out_ports for elseif in self.elseifs]
+        if n_then == n_else and \
+           n_then.count(num_elses_ports) == len(num_elses_ports):
+            raise Exception(
+                f"number of expressions should be equal"
+                f"in all branches of an 'if'{self.location}"
+            )
+        return len(self.then)
+
     def build(self, scope: SisalScope):
         """this recursively rebuilds the if's ir into a dataflow graph"""
 
