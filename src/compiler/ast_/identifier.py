14fe39e19bd34a16b1b74cc777e51b9de2067c37
Alex M <you@example.com>
2022-06-03 14:01:05 +0700

    intermediate

--- src/compiler/ast_/identifier.py
+++ src/compiler/ast_/identifier.py
@@ -9,6 +9,7 @@ from ..scope import SisalScope
 from ..sub_ir import SubIr
 from ..error import SisalError
 
+
 class Identifier(Node):
     """This is an intermediate node, it's deleted in the second pass"""
 
@@ -19,7 +20,7 @@ class Identifier(Node):
         self.name = identifier_name
 
     def __repr__(self):
-        return f"<Identifier: \"{self.name}\" {self.location}"
+        return f'<Identifier: "{self.name}" {self.location}'
 
     def num_out_ports(self):
         """override Node's num_out_ports because we don't need any ports"""
@@ -29,5 +30,7 @@ class Identifier(Node):
     def build(self, target_ports: list[Port], scope: SisalScope):
         self.out_ports = [scope.resolve_by_name(self.name)]
         if self.out_ports == [None]:
-            raise SisalError (f"identidier {self.name} was not defined.", self.location)
+            raise SisalError(
+                f'identifier "{self.name}" was not defined.', self.location
+            )
         return SubIr(nodes=[], output_edges=[], internal_edges=[])
