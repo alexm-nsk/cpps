7540eeadacc3c8717deed6ddafa7826628f48165
alexm-nsk <alex.m.work@yandex.ru>
2022-06-08 17:58:24 +0700

    intermediate

--- src/compiler/ast_/array_access.py
+++ src/compiler/ast_/array_access.py
@@ -11,10 +11,12 @@ from ..scope import SisalScope
 # from ..error import SisalError
 from ..sub_ir import SubIr
 from ..port import Port
-from ..type import IntegerType
+from ..edge import Edge
+from ..type import IntegerType, ArrayType
 
 
 class ArrayAccess(Node):
+
     def __init__(self, array: Node, index: Node, location: str = None):
         super().__init__(location)
         if array:
@@ -25,11 +27,11 @@ class ArrayAccess(Node):
         self.name = "ArrayAccess"
 
         self.in_ports = [
-            Port(self.id, None, 0),  # Array
-            Port(self.id, IntegerType(), 1),  # Index
+            Port(self.id, ArrayType(element = IntegerType()), 0),  # addressed array, type is set later
+            Port(self.id, IntegerType(), 1),  # element's index
         ]
 
-        self.out_ports = [Port(self.id, None, 0)]
+        self.out_ports = [Port(self.id, IntegerType(), 0)]
 
     def num_out_ports(self) -> int:
         return 1
@@ -37,19 +39,22 @@ class ArrayAccess(Node):
     @build_method
     def build(self, target_ports: list[Port], scope: SisalScope) -> SubIr:
 
-        identifier_ir = self.array.build([self.in_ports[0]], scope)
+        array_ir = self.array.build([self.in_ports[0]], scope)
 
-        nodes = []
+        nodes = [self]
+        indices_ir = SubIr([], [], [])
+        edges = []
         for index in self.index:
             aa = ArrayAccess(None, None, self.location)
+            edges.append(Edge(nodes[-1].out_ports[0], aa.in_ports[0]))
             nodes.append(aa)
-            indices_ir = [index.build([self.in_ports[1]], scope)]
+            indices_ir += index.build([aa.in_ports[1]], scope)
 
         del self.array
         del self.index
 
         return (
-            SubIr(nodes=[self], output_edges=[], internal_edges=[])
-            + identifier_ir
-            + index_ir
+            SubIr(nodes=[self], output_edges=[], internal_edges=edges)
+            + array_ir
+            + indices_ir
         )
