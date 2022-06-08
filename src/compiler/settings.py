5d982933fe2021eca4a892fc69f728833c993a66
alexm-nsk <alex.m.work@yandex.ru>
2022-06-08 15:58:41 +0700

    intermediate

--- src/compiler/settings.py
+++ src/compiler/settings.py
@@ -1,3 +1,6 @@
 """various global parameters are stored here"""
 # if a port's label is None, don't store it when exporting an IR
 DONT_ADD_EMPTY_LABELS = True
+# don't use separate node for Let and
+# turn it into a simple set of nodes and edges
+UNPACK_LET = False
