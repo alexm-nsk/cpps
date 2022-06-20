023c61bb105f1b370177759fbfebe7bbd275a177
alexm-nsk <alex.m.work@yandex.ru>
2022-06-20 13:11:23 +0700

    intermediate

--- src/compiler/parse.py
+++ src/compiler/parse.py
@@ -269,19 +269,18 @@ class ModuleVisitor(NodeVisitor):
     def visit_while_do(self, node, vc_):
         return loop.LoopBody(
             statements=vc_[6],
-            condition=loop.PreCond(exp=vc_[2], location=self.get_location(node)),
+            condition=loop.PreCond(exp=vc_[2], self.get_location(node)),
             location=self.get_location(node),
         )
 
     def visit_do_while(self, node, vc_):
         return loop.LoopBody(
             statements=vc_[2],
-            condition=loop.PostCond(exp=vc_[6], location=self.get_location(node)),
+            condition=loop.PostCond(exp=vc_[6], self.get_location(node)),
             location=self.get_location(node),
         )
 
     def visit_repeat(self, node, vc_):
-
         return loop.LoopBody(
             statements=vc_[2], condition=None, location=self.get_location(node)
         )
