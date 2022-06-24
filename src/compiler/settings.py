5802713c6d2c7a96071b0ff91a8298dbee52866e
Alex M <you@example.com>
2022-06-24 21:19:23 +0700

    intermediate

--- src/compiler/settings.py
+++ src/compiler/settings.py
@@ -4,5 +4,6 @@ DONT_ADD_EMPTY_LABELS = True
 # don't use separate node for Let and
 # turn it into a simple set of nodes and edges
 UNPACK_LET = False
-
+# When exporting IR as JSON, copy full description of ports
+# (sisal-cl compatibility)
 PORT_FULL_DESCRIPTION_IN_EDGES = False
