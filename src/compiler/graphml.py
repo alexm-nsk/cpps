7604635af3ef0d14d070b46f97a856b1cb885c22
Alex M <you@example.com>
2022-06-28 20:30:49 +0700

    intermediate

--- src/compiler/graphml.py
+++ src/compiler/graphml.py
@@ -44,3 +44,54 @@ class GraphMlModule:
 
     def __str__(self):
         return self.document()
+
+
+def graphml(self, extra=""):
+    """get gml text of ports, nodes, etc"""
+    gml = GraphMlModule
+
+    gml_content = ""
+
+    # convert edges and nodes:
+
+    for key in ["name", "function_name", "operator", "value"]:
+        if key in self.__dict__:
+            gml_content += gml.key_str(key, self.__dict__[key]) + "\n"
+
+    # convert ports:
+    if hasattr(self, "in_ports"):
+        for i_p in self.in_ports:
+            gml_content += i_p.graphml("in")
+    gml_content += "\n"
+    if hasattr(self, "out_ports"):
+        for o_p in self.out_ports:
+            gml_content += o_p.graphml("out")
+
+    graph_content = ""
+
+    for key in ["else", "elseif", "then", "condition",
+                "body", "init", "returns", "range_gen",
+                "reduction", "branches"]:
+        if key in self.__dict__:
+            if type(self.__dict__[key]) == list:
+                for item in self.__dict__[key]:
+                    graph_content += gml.indent(item.graphml()) + "\n"
+            else:
+                graph_content += gml.indent(self.__dict__[key].graphml()) + "\n"
+
+    if hasattr(self, "nodes"):
+        for node in self.nodes:
+            graph_content += node.graphml() + "\n"
+
+    if hasattr(self, "edges"):
+        for edge in self.edges:
+            graph_content += edge.gml() + "\n"
+
+    if graph_content:
+        graph = f'<graph id="{self.id}_graph" '\
+                'edgedefault="directed">\n'
+        graph += gml.indent(graph_content)
+        graph += "\n</graph>"
+
+        gml_content +="\n" + graph
+    return f"<node id={self.id}>\n{gml.indent(gml_content)}\n</node>"
