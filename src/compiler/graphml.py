53faf7f9e8a7a9d0c3dfc9b4787f92538a9f914c
Alex M <you@example.com>
2022-06-28 15:15:23 +0700

    intermediate

--- src/compiler/graphml.py
+++ src/compiler/graphml.py
@@ -14,7 +14,9 @@ class GraphMlModule:
         '  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n'
         '  xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns\n'
         '    http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">\n\n'
-        '    <key id="type" for="node" attr.name="nodetype" attr.type="string"/>\n'
-        '    <key id="location" for="node" attr.name="location" attr.type="string" />\n\n\n'
+        '    <key id="type" for="node" attr.name="nodetype" '
+        'attr.type="string"/>\n'
+        '    <key id="location" for="node" attr.name="location" '
+        'attr.type="string" />\n\n\n'
         f"  {content}s\n"
         "\n\n</graphml>"
