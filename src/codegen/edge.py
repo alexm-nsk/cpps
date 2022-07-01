b5947ce5bf7d062b23f4c288a3fd674d28fe072f
Alex M <you@example.com>
2022-07-01 14:53:57 +0700

    intermediate

--- src/codegen/edge.py
+++ src/codegen/edge.py
@@ -6,7 +6,12 @@ Edges for code generator
 
 from dataclasses import dataclass
 from .port import Port
-from .type import Type
+# from .type import Type
+
+
+edges = []
+from_edges = {}
+to_edges = {}
 
 
 @dataclass
@@ -15,3 +20,13 @@ class Edge:
     from_: Port
     to: Port
 
+    def __post_init__(self):
+        if self.from_.node_id not in from_edges:
+            from_edges[self.from_.node_id] = []
+
+        if self.to.node_id not in to_edges:
+            to_edges[self.to.node_id] = []
+
+        from_edges[self.from_.node_id].append(self)
+        to_edges[self.to.node_id].append(self)
+        edges.append(self)
