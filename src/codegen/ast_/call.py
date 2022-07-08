7702ea1bc8ea17512a460732d4e5e4c603124953
Alex M <you@example.com>
2022-07-08 17:40:25 +0700

    intermediate

--- src/codegen/ast_/call.py
+++ src/codegen/ast_/call.py
@@ -15,13 +15,15 @@ from ..cpp.cpp_codegen import (
 
 
 class FunctionCall(Node):
-
     def to_cpp(self, scope, block, name=None):
         left = cpp_eval(self.in_ports[0], scope, block)
         result = CppVariable("call", self.out_ports[0].type.cpp_type)
         block.add_variable(result)
-        args = ""
-        block.add_code(CppAssignment(result, f"{self.callee} {args}"))
+
+        arg_vars = [str(cpp_eval(i_p, scope, block)) for i_p in self.in_ports]
+        args = ", ".join(arg_vars)
+
+        block.add_code(CppAssignment(result, f"{self.callee}({args})"))
         self.out_ports[0].value = result
 
 
