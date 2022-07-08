89a5610277750de665a7b7da441d3320b297fcb8
Alex M <you@example.com>
2022-07-08 17:25:22 +0700

    intermediate

--- src/codegen/ast_/call.py
+++ src/codegen/ast_/call.py
@@ -4,12 +4,25 @@
 code generator call
 """
 from ..node import Node
+from ..cpp.cpp_codegen import (
+    CppScope,
+    CppVariable,
+    indent_cpp,
+    CppBlock,
+    cpp_eval,
+    CppAssignment,
+)
 
 
 class FunctionCall(Node):
 
     def to_cpp(self, scope, block, name=None):
-        return ""
+        left = cpp_eval(self.in_ports[0], scope, block)
+        result = CppVariable("call", self.out_ports[0].type.cpp_type)
+        block.add_variable(result)
+        args = ""
+        block.add_code(CppAssignment(result, f"{self.callee} {args}"))
+        self.out_ports[0].value = result
 
 
 class BuiltInFunctionCall(Node):
