85f532d9e01bc1cc44c2da3feb859c817415310e
Alex M <you@example.com>
2022-07-21 14:59:36 +0700

    intermediate

--- src/codegen/ast_/common.py
+++ src/codegen/ast_/common.py
@@ -3,9 +3,15 @@
 """
 code generator init
 """
-from ..node import Node
-
+from ..node import Node, to_cpp_method
+from ..cpp.cpp_codegen import CppBlock, CppVariable
 
 class Init(Node):
-    def to_cpp(self, block):
+
+    copy_parent_input_values = True
+
+    @to_cpp_method
+    def to_cpp(self, block: CppBlock):
+        result = CppVariable("let_result", self.out_ports[0].type)
+        self.out_ports[0].value = result
         return "INIT"
