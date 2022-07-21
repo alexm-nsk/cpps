21bd7a24fefa6bb98a72e283e2f17a01f9b187f9
Alex M <you@example.com>
2022-07-21 16:39:38 +0700

    intermediate

--- src/codegen/ast_/let.py
+++ src/codegen/ast_/let.py
@@ -9,14 +9,17 @@ from ..cpp.cpp_codegen import CppBlock, CppVariable, cpp_eval
 
 class Let(Node):
 
+    copy_parent_input_values = True
+
     @to_cpp_method
     def to_cpp(self, block):
-        result = CppVariable("let_result", self.out_ports[0].type.cpp_type)
-        block.add_variable(result)
-        self.out_ports[0].value = result
+        # result = CppVariable("let_result", self.out_ports[0].type.cpp_type)
+        # block.add_variable(result)
+        # self.out_ports[0].value = result
         self.init.to_cpp(block, self.body.in_ports)
         # self.body.to_cpp(block)
-        self.out_ports[0].value = cpp_eval(self.body.out_ports[0], block)
+        for o_p, b_o_p in zip(self.out_ports, self.body.out_ports):
+            o_p.value = cpp_eval(b_o_p, block)
 
 
 class LetBody(Node):
