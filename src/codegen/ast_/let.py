0113ca7d77700a142c68809185d6118ac815253d
Alex M <you@example.com>
2022-07-21 15:01:58 +0700

    intermediate

--- src/codegen/ast_/let.py
+++ src/codegen/ast_/let.py
@@ -3,11 +3,17 @@
 """
 code generator let
 """
-from ..node import Node
+from ..node import Node, to_cpp_method
+from ..cpp.cpp_codegen import CppBlock, CppVariable
 
 
 class Let(Node):
+
+    @to_cpp_method
     def to_cpp(self, block):
+        result = CppVariable("let_result", self.out_ports[0].type.cpp_type)
+        block.add_variable(result)
+        self.out_ports[0].value = result
         self.init.to_cpp(block)
 
 
