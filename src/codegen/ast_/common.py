6fbf067cd0f31a9d98955aacaeb4e7547b0c7462
Alex M <you@example.com>
2022-07-21 15:42:10 +0700

    intermediate

--- src/codegen/ast_/common.py
+++ src/codegen/ast_/common.py
@@ -9,6 +9,7 @@ from ..cpp.cpp_codegen import CppBlock, CppVariable
 class Init(Node):
 
     def to_cpp(self, block: CppBlock):
-        result = CppVariable("let_result", self.out_ports[0].type)
+        for o_p in self.out_ports:
+            result = CppVariable(o_p.label, o_p.type)
         self.out_ports[0].value = result
         return "INIT"
