ed1bcc5637af6d821677832422391f831f99836e
Alex M <you@example.com>
2022-07-21 12:08:13 +0700

    intermediate

--- src/codegen/ast_/alg.py
+++ src/codegen/ast_/alg.py
@@ -3,7 +3,7 @@
 """
 code generator algebraic operations
 """
-from ..node import Node
+from ..node import Node  # to_cpp_method
 from ..cpp.cpp_codegen import CppVariable, cpp_eval, CppAssignment
 
 
@@ -23,10 +23,9 @@ class Binary(Node):
 class Unary(Node):
 
     def to_cpp(self, block):
-        name = "un"
         operand = cpp_eval(self.in_ports[0], block)
 
-        result = CppVariable(name, self.out_ports[0].type.cpp_type)
+        result = CppVariable("un", self.out_ports[0].type.cpp_type)
         block.add_variable(result)
         block.add_code(
             CppAssignment(result, f"{self.operator} {operand}")
